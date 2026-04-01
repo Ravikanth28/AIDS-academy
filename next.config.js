04fa3d7213e2879e72cfc7513c586d7bab43e10c
Ravikanth <ravikanthsankaran@gmail.com>
2026-04-01 19:01:53 +0530

    fix: add explicit webpack alias for @ to resolve module not found on Render (Linux)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,6 +10,10 @@ const nextConfig = {
   },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
   },
 }
 
